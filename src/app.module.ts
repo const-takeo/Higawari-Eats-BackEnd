9608979951db5cf0e236829c164c3019d67e6861
const-takeo <const.takeo@gmail.com>
2021-01-07 21:50:20 +0900

    TypeORM Setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
@@ -8,6 +9,17 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
       autoSchemaFile: true,
     }),
     RestaurantsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'takeo',
+      password: '0000',
+      database: 'Higawari-Eats',
+      entities: [],
+      synchronize: true,
+      logging: true,
+    }),
   ],
   controllers: [],
   providers: [],
