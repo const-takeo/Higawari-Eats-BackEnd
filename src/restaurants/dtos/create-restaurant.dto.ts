3264feec8d056cedb76d228b61f2040f4e7b3601
const-takeo <const.takeo@gmail.com>
2021-01-07 21:20:26 +0900

    Validate Args

--- src/restaurants/dtos/create-restaurant.dto.ts
+++ src/restaurants/dtos/create-restaurant.dto.ts
@@ -1,19 +1,26 @@
 import { ArgsType, Field } from '@nestjs/graphql';
+import { IsBoolean, IsString, Length } from 'class-validator';
 
 @ArgsType()
 export class CreateRestaurantDto {
+  @IsString()
+  @Length(2, 5)
   @Field((type) => String)
   name: string;
 
+  @IsString()
   @Field((type) => String)
   location: string;
 
+  @IsString()
   @Field((type) => String)
   address: string;
 
+  @IsString()
   @Field((type) => String)
   ownerName: string;
 
+  @IsBoolean()
   @Field((type) => Boolean)
   isVegan: boolean;
 }
