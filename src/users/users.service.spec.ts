14d4ae57605613fab58e4cd952d0bc4a14b9bd07
const-takeo <const.takeo@gmail.com>
2021-01-19 01:28:47 +0900

    Mocking & First User test

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,5 +1,26 @@
 import { Test } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { JwtService } from 'src/jwt/jwt.service';
+import { MailService } from 'src/mail/mail.service';
+import { UserEntity } from './entities/user.entity';
+import { Verification } from './entities/verification.entity';
 import { UsersService } from './users.service';
+
+// fake functionを作る、mock function
+const mockRepository = {
+  findOne: jest.fn(),
+  save: jest.fn(),
+  create: jest.fn(),
+};
+// mock func of jwt
+const mockJwtService = {
+  sign: jest.fn(),
+  verify: jest.fn(),
+};
+// mock func of mail
+const mockMailService = {
+  sendVerificationEmail: jest.fn(),
+};
 
 //jest
 describe('UsersService', () => {
@@ -8,7 +29,26 @@ describe('UsersService', () => {
   beforeAll(async () => {
     const module = await Test.createTestingModule({
       //モジュールにimportしたいことを書く
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        //mockRepository => typeOrmで本当のrepositoryを呼び出すのではなく偽物を作る。
+        {
+          provide: getRepositoryToken(UserEntity),
+          useValue: mockRepository,
+        },
+        {
+          provide: getRepositoryToken(Verification),
+          useValue: mockRepository,
+        },
+        {
+          provide: JwtService,
+          useValue: mockJwtService,
+        },
+        {
+          provide: MailService,
+          useValue: mockMailService,
+        },
+      ],
     }).compile();
     service = module.get<UsersService>(UsersService);
   });
