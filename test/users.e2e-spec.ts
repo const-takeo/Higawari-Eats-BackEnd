8649a03a442843e0a8f9b644e33dfbb16c6afa14
const-takeo <const.takeo@gmail.com>
2021-01-23 13:27:30 +0900

    Finished E2E Test Setup

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import { AppModule } from '../src/app.module';
+import { getConnection } from 'typeorm';
 
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
@@ -13,6 +14,18 @@ describe('UserModule (e2e)', () => {
     app = module.createNestApplication();
     await app.init();
   });
-
+  //test後閉じ無いとエラが起こる。
+  afterAll(async () => {
+    //testが終わるとDBをdropする。
+    await getConnection().dropDatabase();
+    app.close();
+  });
+  //todo
+  //testの順番を付けるのは重要。
+  it.todo('createAccount');
+  it.todo('userProfile');
+  it.todo('login');
   it.todo('me');
+  it.todo('verifyEmail');
+  it.todo('editProfile');
 });
