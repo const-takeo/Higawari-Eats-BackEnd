7b45f0190a5ea1bfd39ab45da84d2abbecb15d81
const-takeo <const.takeo@gmail.com>
2021-03-05 01:28:23 +0900

    AWS S3

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   // app.use(JwtMiddleware);
+  // localhost 3000 -request-> localhost 4000 acces denyを解決
+  app.enableCors();
   await app.listen(4000);
 }
 bootstrap();
--- src/uploads/uploads.controller.ts
+++ src/uploads/uploads.controller.ts
@@ -5,12 +5,39 @@ import {
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import * as AWS from 'aws-sdk';
+
+//DONT FORGET THIS BUCKET NAME
+//bucketは最初に一回だけ作れば良い
+//It needs unique name
+const BUCKET_NAME = 'kimchinattobutaitameoishii';
 
 @Controller('uploads')
 export class UploadsController {
   @Post('')
   @UseInterceptors(FileInterceptor('file'))
-  uploadFile(@UploadedFile() file) {
-    console.log(file);
+  async uploadFile(@UploadedFile() file) {
+    AWS.config.update({
+      credentials: {
+        accessKeyId: 'AKIAWLYLM5Z3G2I3M4XG',
+        secretAccessKey: '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',
+      },
+    });
+    try {
+      const objectName = `${Date.now() + file.originalname}`;
+      await new AWS.S3()
+        .putObject({
+          Body: file.buffer,
+          Bucket: BUCKET_NAME,
+          //It needs unique name
+          Key: objectName,
+          ACL: 'public-read',
+        })
+        .promise();
+      const url = `https://${BUCKET_NAME}.s3.amazonaws.com/${objectName}`;
+      return { url };
+    } catch (error) {
+      return null;
+    }
   }
 }
